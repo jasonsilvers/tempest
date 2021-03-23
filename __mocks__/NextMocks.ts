e38534fe697b17dd2a83aed491faf29e3e39c642
Patrick Kelly <patrick.kelly.22@us.af.mil>
2021-03-23 12:47:01 -1000

    added types to the useTestUser mock

--- __mocks__/NextMocks.ts
+++ __mocks__/NextMocks.ts
@@ -1,6 +1,8 @@
+import { User } from '@prisma/client';
+import { IUserContext } from '@tron/nextjs-auth-p1/dist/client/UserContextProvider';
+
 export const useTestRouter = jest.spyOn(require('next/router'), 'useRouter');
 
-export const useTestUser = jest.spyOn(
-  require('@tron/nextjs-auth-p1'),
-  'useUser'
-);
+export const useTestUser: jest.SpyInstance<
+  IUserContext<Partial<User>>
+> = jest.spyOn(require('@tron/nextjs-auth-p1'), 'useUser');
--- components/Navigation/tests/Navbar.test.tsx
+++ components/Navigation/tests/Navbar.test.tsx
@@ -7,6 +7,8 @@ import { useTestRouter, useTestUser } from '../../../__mocks__/NextMocks';
 test('should render a navbar', () => {
   useTestUser.mockImplementationOnce(() => ({
     user: { name: 'test user' },
+    isError: false,
+    isLoading: false,
   }));
   useTestRouter.mockImplementation(() => ({
     route: '/',
@@ -15,5 +17,5 @@ test('should render a navbar', () => {
   const header = getByText(/Tempest/i);
   expect(header).toBeInTheDocument;
 
-  //   expect(getByText(/test user/i)).toBeInTheDocument;
+  expect(getByText(/test user/i)).toBeInTheDocument;
 });
