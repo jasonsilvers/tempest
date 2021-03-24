62ca5b7c77452ce5ad7c1fce81dd0ccb2473c2d3
Jason Silvers <jason.silvers.2@us.af.mil>
2021-03-24 00:41:54 +0000

    Add methods to check permissions on API and UI

---
+++ hooks/usePermissions.tsx
@@ -0,0 +1,32 @@
+import { useUser } from "@tron/nextjs-auth-p1";
+import { AccessControl } from "accesscontrol";
+import axios from "axios";
+import React, { useMemo } from "react";
+import { useQuery } from "react-query";
+import { UserWithRole } from "../prisma/repositories/user";
+import { Role } from "../types/global";
+
+const usePermissions = () => {
+  const { user } = useUser<UserWithRole>();
+
+  const grantsQuery = useQuery(
+    "grants",
+    () => axios.get("/api/grants").then((result) => result.data),
+    {
+      enabled: !!user,
+      staleTime: Infinity,
+    }
+  );
+
+  let ac: AccessControl;
+  let userRole: string;
+
+  if (grantsQuery.data) {
+    userRole = user.role.accessControlName;
+    ac = new AccessControl(grantsQuery.data);
+  }
+
+  return { ...grantsQuery, ac, userRole };
+};
+
+export default usePermissions;
--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import {withPageAuth} from '@tron/nextjs-auth-p1'
+import React from "react";
+import { useUser, withPageAuth } from "@tron/nextjs-auth-p1";
+import { Resource, Role } from "../types/global";
+import usePermissions from "../hooks/usePermissions";
+import { UserWithRole} from "../prisma/repositories/user";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { ac, isLoading, userRole } = usePermissions();
+
+  const permission = ac?.can(userRole).read(Resource.PROFILE);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (permission?.granted) {
+    return <h1>Profile</h1>;
+  }
+
+  return <h1>You do not have access to this page</h1>;
 };
 
 export default withPageAuth(Profile);
