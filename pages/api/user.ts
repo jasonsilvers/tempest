45a93cd5d77e1692d73ec08d6f09a356ce46ea14
Patrick Kelly <patrick.kelly.22@us.af.mil>
2021-04-06 14:08:04 -1000

    mocked repo spyon added to project

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -4,32 +4,28 @@ import {
   NextApiRequestWithAuthorization,
 } from '@tron/nextjs-auth-p1';
 import { User } from '@prisma/client';
-import { getUser } from '../../prisma/repositories/user';
-import prisma from '../../prisma/prisma';
+import { getUserByDodId, postUser } from '../../prisma/repositories/user';
 
 export const userApi = async (
   req: NextApiRequestWithAuthorization<User>,
   res: NextApiResponse
 ) => {
-  const { body, method } : { body:User, method?: string } = req;
-  const user = await prisma.user.create({
-      data: body,
-    });
+  const { body, method }: { body: User; method?: string } = req;
   switch (method) {
     case 'POST': {
       if (body.id) {
         res.status(400).end(`ID must be null`);
         break;
       }
+      res.json(postUser(body));
       res.statusCode = 200;
-      res.json(user);
       break;
     }
 
     default:
-      res.setHeader('Allow', ['POST', 'PUT']);
+      res.setHeader('Allow', ['POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
 
-export default withApiAuth(userApi, getUser);
+export default withApiAuth(userApi, getUserByDodId);
--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,6 +1,7 @@
 import { User } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../prisma/prisma';
+import { getUserById, putUser } from '../../../prisma/repositories/user';
 
 export default async function userHandler(
   req: NextApiRequest,
@@ -9,6 +10,7 @@ export default async function userHandler(
   const {
     query: { id },
     method,
+    body,
   } = req;
 
   let userId = 0;
@@ -19,23 +21,14 @@ export default async function userHandler(
 
   switch (method) {
     case 'GET': {
-      const user = await prisma.user.findUnique({
-        where: { id: userId },
-      });
+      const user = await getUserById(userId);
+
       res.status(200).json(user);
       break;
     }
-    case 'POST':
-      break;
     case 'PUT': {
-      const { name, email } = req.body;
-      const result = await prisma.user.create({
-        data: {
-          name,
-          email,
-        },
-      });
-      res.status(200).json(result);
+      const user = await putUser(body);
+      res.status(200).json(user);
       break;
     }
     default:
