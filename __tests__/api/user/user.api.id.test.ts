dea7f6f3f55dee34460562d303356c5efed9a45f
Patrick Kelly <patrick.kelly.22@us.af.mil>
2021-04-06 15:05:14 -1000

    repository for user created and tested, user endpoints created for GET/id PUT/id POST

--- __tests__/api/user/user.api.id.test.ts
+++ __tests__/api/user/user.api.id.test.ts
@@ -40,6 +40,7 @@ test('api/user/1:PUT--Happy Case', async () => {
   const { req, res } = createMocks({
     method: 'PUT',
     body: { ...userTest, id: 1 },
+    query: { id: '1' },
   });
 
   await userHandler(req, res);
@@ -49,9 +50,10 @@ test('api/user/1:PUT--Happy Case', async () => {
   expect(res._getStatusCode()).toBe(200);
   expect(JSON.parse(res._getData())).toEqual(expectedUser);
   expect(spy).toHaveBeenCalledWith({ ...userTest, id: 1 });
+  spy.mockClear();
 });
 
-test('api/user/1:PUT--', async () => {
+test('api/user/1:PUT--Body Null Id', async () => {
   const spy = mockRepository<UserWithRole, typeof repo>(repo, 'putUser', {
     ...userTest,
     id: 1,
@@ -60,13 +62,47 @@ test('api/user/1:PUT--', async () => {
   const { req, res } = createMocks({
     method: 'PUT',
     body: userTest,
+    query: { id: '1' },
   });
 
   await userHandler(req, res);
 
-  const expectedUser = { ...userTest, id: 1 };
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getData()).toEqual('User must have id to update');
+  expect(spy).not.toHaveBeenCalledWith(userTest);
+  spy.mockClear();
+});
 
-  expect(res._getStatusCode()).toBe(200);
-  expect(JSON.parse(res._getData())).toEqual(expectedUser);
-  expect(spy).toHaveBeenCalledWith(userTest);
+test('api/user/1:PUT--Query and Body ids', async () => {
+  const spy = mockRepository<UserWithRole, typeof repo>(repo, 'putUser', {
+    ...userTest,
+    id: 1,
+  });
+
+  const { req, res } = createMocks({
+    method: 'PUT',
+    body: { ...userTest, id: 2 },
+    query: { id: '1' },
+  });
+
+  await userHandler(req, res);
+
+  expect(spy).toHaveBeenCalledTimes(0);
+  expect(res._getStatusCode()).toBe(400);
+  expect(res._getData()).toEqual('User Obj and Query must match for id');
+  spy.mockClear();
+});
+
+test('api/user/1:POST--Method not allowed', async () => {
+  const { req, res } = createMocks({
+    method: 'POST',
+    body: { ...userTest, id: 1 },
+    query: { id: 1 },
+  });
+  await userHandler(req, res);
+
+  const expectedUserError = 'Method POST Not Allowed';
+
+  expect(res._getStatusCode()).toBe(405);
+  expect(res._getData()).toEqual(expectedUserError);
 });
---
+++ __tests__/repository/user.repo.test.ts
@@ -0,0 +1,51 @@
+import { User } from '.prisma/client';
+import {
+  getUserByDodId,
+  getUserById,
+  postUser,
+  putUser,
+} from '../../prisma/repositories/user';
+
+const mockUser: Partial<User> = { name: 'Bob', dodId: '1234', id: 1 };
+jest.mock('@prisma/client', () => {
+  return {
+    PrismaClient: function () {
+      return {
+        user: {
+          findUnique: () => mockUser,
+          create: () => mockUser,
+          update: () => mockUser,
+        },
+      };
+    },
+  };
+});
+
+afterAll = () => {
+  jest.clearAllMocks();
+};
+
+const user: Partial<User> = { name: 'Bob', dodId: '1234' };
+test('Repository User Prisma Mock Test for Create', async () => {
+  const returnPostUser = await postUser(user as User);
+
+  expect(returnPostUser).toStrictEqual({ ...user, id: 1 });
+});
+
+test('Repository User Prisma Mock Test for Update', async () => {
+  const returnPutUser = await putUser({ ...user, id: 1 } as User);
+
+  expect(returnPutUser).toStrictEqual({ ...user, id: 1 });
+});
+
+test('Repository User Prisma Mock Test for Get By Id', async () => {
+  const returnIdUser = await getUserById(1);
+
+  expect(returnIdUser).toStrictEqual({ ...user, id: 1 });
+});
+
+test('Repository User Prisma Mock Test for Get By DoD Id', async () => {
+  const returnDoDIdUser = await getUserByDodId('1234');
+
+  expect(returnDoDIdUser).toStrictEqual({ ...user, id: 1 });
+});
