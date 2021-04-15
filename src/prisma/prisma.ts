4fa66e3f121257369821a6a1884160a23120c998
Jason Silvers <jason.silvers.2@us.af.mil>
2021-04-14 15:42:23 -1000

    remove jest.fn from prisma.ts file

--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -3,14 +3,10 @@ import { PrismaClient } from '@prisma/client';
 // Docs about instantiating `PrismaClient` with Next.js:
 // https://pris.ly/d/help/next-js-best-practices
 
-let prisma: PrismaClient | any;
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
-}
-
-if (process.env.NODE_ENV === 'test') {
-  prisma = jest.fn();
 }
 
 if (process.env.NODE_ENV === 'development') {
