49641569ef5a000d2370d5cc0a7b298462621311
Patrick Kelly <patrick.kelly.22@us.af.mil>
2021-05-20 17:28:26 -1000

    Index.test.ts 100%

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,22 @@
+import { render, waitFor } from '../utils/TempestTestUtils';
+import Index from '../../src/pages/index';
+import { useTestUser } from '../utils/mocks/NextMocks';
+import { ERole } from '../../src/types/global';
+
+it('routes the user to the Unauthenticated app', async () => {
+  const push = jest.fn();
+  render(<Index />, { push });
+  expect(push).toBeCalledTimes(1);
+  expect(push).toBeCalledWith('/Unauthenticated');
+});
+
+it('routes the user to the Unauthenticated app when loading', () => {
+  useTestUser.mockImplementationOnce(() => ({
+    isLoading: true,
+    user: { firstName: 'bob', role: { name: ERole.MEMBER, id: 1 } },
+  }));
+  const push = jest.fn();
+  render(<Index />, { push });
+  expect(push).toBeCalledTimes(1);
+  expect(push).toBeCalledWith('/Profile');
+});
--- __tests__/utils/mocks/NextMocks.ts
+++ __tests__/utils/mocks/NextMocks.ts
@@ -1,7 +1,7 @@
-import { User } from '@prisma/client';
 import { IUserContext } from '@tron/nextjs-auth-p1/dist/client/UserContextProvider';
 import * as router from 'next/router';
 import { NextRouter } from 'next/router';
+import { UserWithRole } from '../../../src/repositories/userRepo';
 
 export const useTestRouter: jest.SpyInstance<Partial<NextRouter>> = jest.spyOn(
   router,
@@ -9,6 +9,5 @@ export const useTestRouter: jest.SpyInstance<Partial<NextRouter>> = jest.spyOn(
 );
 
 export const useTestUser: jest.SpyInstance<
-  IUserContext<Partial<User>>
+  IUserContext<Partial<UserWithRole>>
 > = jest.spyOn(require('@tron/nextjs-auth-p1'), 'useUser');
-
