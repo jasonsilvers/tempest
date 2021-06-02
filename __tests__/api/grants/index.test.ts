0d98d881f48fcfc19c07b4cbee58f3238d36739d
Jason Silvers <jason.silvers.2@us.af.mil>
2021-06-02 22:20:42 +0000

    Chore/tests api grants

---
+++ __tests__/api/grants/index.test.ts
@@ -0,0 +1,36 @@
+import testNextApi from '../../utils/NextAPIUtils';
+import grantsHandler from '../../../src/pages/api/grants';
+import { mockMethodAndReturn } from '../../utils/mocks/repository';
+import { findUserByDodId } from '../../../src/repositories/userRepo';
+import { findGrants } from '../../../src/repositories/grantsRepo';
+import { grants } from '../../utils/mocks/fixtures';
+
+jest.mock('../../../src/repositories/userRepo.ts');
+jest.mock('../../../src/repositories/grantsRepo.ts');
+
+const globalUserId = 'a100e2fa-50d0-49a6-b10f-00adde24d0c2';
+
+beforeEach(() => {
+  mockMethodAndReturn(findUserByDodId, {
+    id: globalUserId,
+    firstName: 'joe',
+    role: { id: '22', name: 'monitor' },
+  });
+  mockMethodAndReturn(findGrants, grants);
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('should return grants', async () => {
+  const { status, data } = await testNextApi.get(grantsHandler);
+
+  expect(status).toBe(200);
+  expect(data).toStrictEqual(grants);
+});
+test('should return 401 if not authorized', async () => {
+  const { status } = await testNextApi.get(grantsHandler, { withJwt: false });
+
+  expect(status).toBe(401);
+});
---
+++ src/pages/api/grants/index.ts
@@ -0,0 +1,15 @@
+import { NextApiResponse } from 'next';
+import { withApiAuth, NextApiRequestWithAuthorization } from '@tron/nextjs-auth-p1';
+import { User } from '@prisma/client';
+import { findUserByDodId } from '../../../repositories/userRepo';
+import { findGrants } from '../../../repositories/grantsRepo';
+
+const grantsHandler = async (req: NextApiRequestWithAuthorization<User>, res: NextApiResponse) => {
+  res.statusCode = 200;
+
+  const grants = await findGrants();
+
+  res.json(grants);
+};
+
+export default withApiAuth(grantsHandler, findUserByDodId);
