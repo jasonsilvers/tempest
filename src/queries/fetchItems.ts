28a2cca27c1e27b853b0c2e0fb0c7c32049ec38a
Jason Silvers <jason.silvers.2@us.af.mil>
2021-06-02 12:43:29 -1000

    removed unused files

--- src/queries/fetchItems.ts
+++
@@ -1,7 +0,0 @@
-import { TrackingItem } from "@prisma/client"
-
-export const fetchItems = async (): Promise<TrackingItem[]> => {
-  const response = await fetch('/api/training')
-  const data: TrackingItem[] = await response.json()
-  return data
-};
