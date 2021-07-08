d92b7d7dc2cd25178b3e19e90111f75a84f7edd6
Patrick Kelly <snekcode@Patricks-MacBook-Pro.local>
2021-07-07 15:01:51 -1000

    null file protection

--- src/pages/api/excel/index.ts
+++ src/pages/api/excel/index.ts
@@ -2,11 +2,15 @@ import formidable from 'formidable';
 import fs from 'fs';
 import Excel from 'exceljs';
 
+// BodyParser is disabled (Next.js enables this by default).
+// Otherwise, formidable is unable to process the files and the files are not placed in the request.
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+// Save file function using fs to store a copy of the uploaded file
 const saveFile = (file) => {
   const path = `./uploads/${file.name}`;
   const data = fs.readFileSync(file.path);
@@ -15,6 +19,7 @@ const saveFile = (file) => {
   return path;
 };
 
+// clean up function to remove any filepath provided
 const deleteFile = async (filepath) => {
   fs.unlinkSync(filepath);
 };
@@ -24,12 +29,20 @@ const handleExcel = async (req, res) => {
     console.log('not a post');
     return res.status(404).send('');
   }
-
+  // create the form using formidable
   const form = new formidable.IncomingForm();
+  // create the Excel workbook form exceljs
   const workbook = new Excel.Workbook();
 
+  // parse the form.
+  // this is where all the logic will reside
   form.parse(req, async function (err, fields, files) {
     let cellValue = undefined;
+    // check for null file uploaded
+    if (!files.file) {
+      res.status(400).json({ error: 'No File Sent' });
+      return;
+    }
     const fp = saveFile(files.file);
 
     workbook.xlsx.readFile(fp).then((openFile) => {
@@ -40,6 +53,8 @@ const handleExcel = async (req, res) => {
       res.json({ message: cellValue });
     });
   });
+
+  return res;
 };
 
 export default handleExcel;
