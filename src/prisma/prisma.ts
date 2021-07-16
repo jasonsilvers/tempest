18bf1092a95308ba24b572212d3235bb7742c639
Patrick Kelly <snekcode@Patricks-MacBook-Pro.local>
2021-07-15 14:56:35 -1000

    build stage fix

--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -1,41 +1,19 @@
-// import { PrismaClient } from '@prisma/client';
-
-// // Docs about instantiating `PrismaClient` with Next.js:
-// // https://pris.ly/d/help/next-js-best-practices
-
-// let prisma: PrismaClient;
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// }
-
-// if (process.env.NODE_ENV === 'development') {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
-// export default prisma;
-
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-type GlobalThisWithPrismaClient = typeof globalThis & {
-  [prismaClientPropertyName]: PrismaClient;
-};
+// Docs about instantiating `PrismaClient` with Next.js:
+// https://pris.ly/d/help/next-js-best-practices
 
-const getPrismaClient = () => {
-  if (process.env.NODE_ENV === `production`) {
-    return new PrismaClient();
-  } else {
-    const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-    if (!newGlobalThis[prismaClientPropertyName]) {
-      newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-    }
-    return newGlobalThis[prismaClientPropertyName];
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+}
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
-};
-const prisma = getPrismaClient();
+  prisma = global.prisma;
+}
 
 export default prisma;
