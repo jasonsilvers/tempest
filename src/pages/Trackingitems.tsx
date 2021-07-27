07457cbf64f9c39ad0c256c119bb8e3aa5abd509
Patrick Kelly <snekcode@Patricks-MacBook-Pro.local>
2021-07-26 17:45:07 -1000

    search bar

--- src/pages/Trackingitems.tsx
+++ src/pages/Trackingitems.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'twin.macro';
 import { tiQueryKeys, useAddTrackingItem, useDeleteTrackingItem, useTrackingItems } from '../hooks/api/trackingItem';
 import { QueryClient } from 'react-query';
 import prisma from '../prisma/prisma';
 import { dehydrate } from 'react-query/hydration';
 import { TrainingItemRow, TrainingItemHeader } from '../components/TrainingItems/TrainingItemRow';
+import SearchBar from '../components/Search/SearchBar';
 
 const H1 = tw.h1`text-xl text-primary font-family[DM Sans] mb-2`;
 
@@ -12,14 +13,22 @@ const TrackingItems = () => {
   const { data: trackingItems } = useTrackingItems();
   const { mutate: create, isLoading: isLoadingCreate } = useAddTrackingItem();
   const { mutate: del, isLoading: isLoadingDelete } = useDeleteTrackingItem();
+  const [search, setSearch] = useState('');
 
   return (
     <>
       <H1>Global Training Catalog {isLoadingCreate || isLoadingDelete ? '...loading' : null}</H1>
+      <SearchBar
+        value={search}
+        onChange={(searchValue) => setSearch(searchValue)}
+        onCancelSearch={() => setSearch('')}
+      />
       <TrainingItemHeader />
-      {trackingItems?.map((trackingItem) => (
-        <TrainingItemRow key={trackingItem.id} trackingItem={trackingItem} />
-      ))}
+      {trackingItems
+        ?.filter((item) => item.title.toLowerCase().includes(search.toLowerCase()))
+        .map((trackingItem) => (
+          <TrainingItemRow key={trackingItem.id} trackingItem={trackingItem} />
+        ))}
     </>
   );
 };
