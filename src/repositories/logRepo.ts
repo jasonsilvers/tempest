b8532c3c5e9295bf173444181d10116a16b344d5
Jason Silvers <jason.silvers.2@us.af.mil>
2021-08-30 14:38:57 -1000

    remove logs from log repo

--- src/repositories/logRepo.ts
+++ src/repositories/logRepo.ts
@@ -3,19 +3,15 @@ import { ELogEventType } from '../types/global';
 import prisma from '../prisma/prisma';
 
 const createLog = async (user: User, logEventType: ELogEventType, message: string) => {
-  try {
-    console.log(user.id, logEventType, message);
-    await prisma?.logEvent.create({
-      data: {
-        userId: user.id,
-        logEventType,
-        message,
-      },
-    });
-  } catch (e) {
-    console.log('it is failing to log the user');
-    console.log(e);
-  }
+  const userId = user ? user.id : null;
+
+  await prisma?.logEvent.create({
+    data: {
+      userId,
+      logEventType,
+      message,
+    },
+  });
 };
 
 export { createLog };
