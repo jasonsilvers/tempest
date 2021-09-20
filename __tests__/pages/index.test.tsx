05b2b64c4c1c03d74c0e7f908bb29ae7d863b4b8
Jason Silvers <jason.silvers.2@us.af.mil>
2021-09-20 12:04:04 -1000

    fix breaking tests, test if member has orgId and routing works

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -10,10 +10,23 @@ it('routes the user to the Unauthenticated app', async () => {
   expect(push).toBeCalledWith('/Unauthenticated');
 });
 
+it('routes the user to the Welcom page if they do not have an organization', () => {
+  useTestUser.mockImplementationOnce(() => ({
+    isLoading: true,
+    user: { firstName: 'bob', id: '1', role: { name: ERole.MEMBER, id: 1 } },
+    refreshUser: () => {},
+  }));
+  const push = jest.fn();
+  render(<Index />, { push });
+  expect(push).toBeCalledTimes(1);
+  expect(push).toBeCalledWith('/Welcome');
+});
+
 it('routes the user to the Profile page if they have the role member', () => {
   useTestUser.mockImplementationOnce(() => ({
     isLoading: true,
-    user: { firstName: 'bob', id: 1, role: { name: ERole.MEMBER, id: 1 } },
+    user: { firstName: 'bob', id: '1', role: { name: ERole.MEMBER, id: 1 }, organizationId: 'adioj3' },
+    refreshUser: () => {},
   }));
   const push = jest.fn();
   render(<Index />, { push });
@@ -24,7 +37,8 @@ it('routes the user to the Profile page if they have the role member', () => {
 it('routes the user to the Dashboard page if their role is not Member', () => {
   useTestUser.mockImplementationOnce(() => ({
     isLoading: true,
-    user: { firstName: 'bob', role: { name: ERole.MONITOR, id: 1 } },
+    user: { firstName: 'bob', role: { name: ERole.MONITOR, id: 1 }, organizationId: 'aji32' },
+    refreshUser: () => {},
   }));
   const push = jest.fn();
   render(<Index />, { push });
