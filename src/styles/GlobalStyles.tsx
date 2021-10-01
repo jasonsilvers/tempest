fa93523fafbb7307e4e897a1dbf67db272dd6434
Jason Silvers <jason.silvers.2@us.af.mil>
2021-09-30 14:15:10 -1000

    clean up global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro';
 
-const color = theme`colors.purple.500`;
 const fontFamily = theme`fontFamily.default`;
 const antialiased = tw`antialiased`;
 
@@ -10,8 +9,7 @@ const CustomStyles = createGlobalStyle`
   body {
     font-family: ${fontFamily};
     background-color: #E5E5E5;
-    color: ${color}
-    ${antialiased}
+    ${antialiased};
   }
 `;
 
