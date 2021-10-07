ec9958d5f42037268dcabb3ee99510d36a2b4b60
Jason Silvers <jason.silvers.2@us.af.mil>
2021-10-07 10:35:29 -1000

    baseline staging database

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ set -x
 node timeout.js
 
 # The normal production work flow
+npx prisma migrate resolve --applied 20210504192826_init
 npx prisma migrate deploy
 
 # npx prisma db push --force-reset --accept-data-loss
