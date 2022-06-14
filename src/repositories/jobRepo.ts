ab5cdab6e286b94ff206858244c9e394016c4f41
Jason Silvers <jason.silvers.2@us.af.mil>
2022-06-14 06:11:06 -1000

    continued work

--- src/repositories/jobRepo.ts
+++ src/repositories/jobRepo.ts
@@ -1,13 +1,14 @@
 import { JobStatus } from '@prisma/client';
 import dayjs from 'dayjs';
 
-export const createJob = (total: number) => {
+export const createJob = (total: number, userId: number) => {
   return prisma.job.create({
     data: {
       message: `Started at ${dayjs().format('TTTT')}`,
       progress: 0,
       status: JobStatus.WORKING,
       total,
+      startedById: userId,
     },
   });
 };
