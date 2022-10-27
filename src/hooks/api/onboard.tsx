4bb893aec5e121cabd5786ab91ec239c0b04b29b
zacharyhunn <zach.hunn@trongov.us>
2022-10-26 15:31:04 -1000

    fixed routing issue onboard page

--- src/hooks/api/onboard.tsx
+++ src/hooks/api/onboard.tsx
@@ -1,25 +1,22 @@
-import { Organization } from "@prisma/client";
-import axios, {AxiosResponse} from "axios";
-import { useSnackbar } from "notistack";
-import { useMutation, useQueryClient } from "react-query";
-import { EUri } from "../../const/enums";
+import { Organization } from '@prisma/client';
+import axios, { AxiosResponse } from 'axios';
+import { useSnackbar } from 'notistack';
+import { useMutation, useQueryClient } from 'react-query';
+import { EUri } from '../../const/enums';
 import { organizationQueryKeys } from './organizations';
 
-
 export const useOnboardOrg = () => {
-    const queryClient = useQueryClient();
-    const snackbar = useSnackbar();
-    
-    return useMutation<AxiosResponse<Organization>, unknown, Organization>(
-        (newOrganization: Organization) => axios.post<Organization>(EUri.ONBOARD, newOrganization),
-        {
-            onError: () => {
-                snackbar.enqueueSnackbar('Error Onboarding. Please try again!', { variant: 'error' });
-            },
-            onSettled: () => {
-                queryClient.invalidateQueries(organizationQueryKeys.organizations());
-            },
-        }
-    )
-
-}
+  const queryClient = useQueryClient();
+  const snackbar = useSnackbar();
+  return useMutation<AxiosResponse<Organization>, unknown, Organization>(
+    (newOrganization: Organization) => axios.post<Organization>(EUri.ONBOARD, newOrganization),
+    {
+      onError: () => {
+        snackbar.enqueueSnackbar('Error Onboarding. Please try again!', { variant: 'error' });
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(organizationQueryKeys.organizations());
+      },
+    }
+  );
+};
--- src/pages/Onboard.tsx
+++ src/pages/Onboard.tsx
@@ -10,116 +10,119 @@ import { useForm } from 'react-hook-form';
 import 'twin.macro';
 import { useOnboardOrg } from '../hooks/api/onboard';
 import { useState } from 'react';
+import { useUser } from '@tron/nextjs-auth-p1';
 
 const formSchema = Joi.object({
-  name: Joi.string().required(),
-  shortName: Joi.string().required(),
+    name: Joi.string().required(),
+    shortName: Joi.string().required(),
 });
 
 const Onboard: FC = () => {
-  const [dialogIsOpen, setDialogIsOpen] = useState(true);
-  const { mutate: onboardOrg } = useOnboardOrg();
-  const { enqueueSnackbar } = useSnackbar();
-  const router = useRouter();
+    const [dialogIsOpen, setDialogIsOpen] = useState(true);
+    const { refreshUser } = useUser()
+    const { mutate: onboardOrg } = useOnboardOrg();
+    const { enqueueSnackbar } = useSnackbar();
+    const router = useRouter();
 
-  const {
-    handleSubmit,
-    reset,
-    register,
-    formState: { errors, dirtyFields },
-  } = useForm({
-    resolver: joiResolver(formSchema),
-    defaultValues: {
-      name: '',
-      shortName: '',
-    },
-  });
-
-  const resetValues = () => {
-    reset({ name: '', shortName: '' });
-  };
-  const createDisabled = Object.keys(dirtyFields).length === 0;
-
-  const submitForm = (data: Organization) => {
-    const createdOrg = {
-      name: data.name,
-      shortName: data.shortName,
-    } as Organization;
-
-    onboardOrg(createdOrg, {
-      onSuccess: () => {
-        enqueueSnackbar('Organization Created', { variant: 'success' });
-        setDialogIsOpen(false);
-        router.push('/Programadmin');
-      },
+    const {
+        handleSubmit,
+        reset,
+        register,
+        formState: { errors, dirtyFields },
+    } = useForm({
+        resolver: joiResolver(formSchema),
+        defaultValues: {
+            name: '',
+            shortName: '',
+        },
     });
-  };
 
-  return (
-    <Dialog
-      open={dialogIsOpen}
-      onClose={() => {
-        resetValues();
-        setDialogIsOpen(false);
-      }}
-      maxWidth="sm"
-      fullScreen
-      aria-labelledby="create-org-dialog"
-    >
-      <div tw="pt-7 pl-6">
-        <IconButton
-          size="small"
-          tw="float-left relative"
-          color="secondary"
-          data-testid="backButton"
-          onClick={() => {
-            resetValues();
-            setDialogIsOpen(false);
-            router.push('/');
-          }}
+    const resetValues = () => {
+        reset({ name: '', shortName: '' });
+    };
+    const createDisabled = Object.keys(dirtyFields).length === 0;
+
+    const submitForm = (data: Organization) => {
+        const createdOrg = {
+            name: data.name,
+            shortName: data.shortName,
+        } as Organization;
+
+        onboardOrg(createdOrg, {
+            onSuccess: () => {
+                enqueueSnackbar('Organization Created', { variant: 'success' });
+                setDialogIsOpen(false);
+                refreshUser()
+                router.push('/Programadmin');
+            },
+        });
+    };
+
+    return (
+        <Dialog
+            open={dialogIsOpen}
+            onClose={() => {
+                resetValues();
+                setDialogIsOpen(false);
+            }}
+            maxWidth="sm"
+            fullScreen
+            aria-labelledby="create-org-dialog"
         >
-          <ArrowBackIcon tw="pr-1" /> BACK
-        </IconButton>
-      </div>
-      <Box tw="w-1/3 h-1/3 border shadow rounded-lg justify-center mx-auto my-auto">
-        <DialogTitle tw="text-3xl text-secondary text-center pt-10">Create Your New Organization</DialogTitle>
-        <DialogContent tw="min-height[220px]">
-          <form id="create-form" onSubmit={handleSubmit(submitForm)} tw="flex flex-col space-y-5 pt-4 items-center">
-            <FormControl fullWidth error={!!errors.name}>
-              <TextField
-                tw="w-2/3 mx-auto"
-                error={!!errors.name}
-                size="small"
-                label="Organization Name"
-                inputProps={{ ...register('name'), 'aria-label': 'org-name' }}
-              />
-            </FormControl>
-            <FormControl fullWidth error={!!errors.shortName}>
-              <TextField
-                tw="w-2/3 mx-auto"
-                error={!!errors.shortName}
-                size="small"
-                label="Short Name"
-                inputProps={{ ...register('shortName'), 'aria-label': 'org-shortName' }}
-              />
-            </FormControl>
-
-            <div tw="flex-col">
-              <Button
-                variant="contained"
-                color="secondary"
-                type="submit"
-                form="create-form"
-                data-testid="createButton"
-                disabled={createDisabled}
-              >
-                Create
-              </Button>
+            <div tw="pt-7 pl-6">
+                <IconButton
+                    size="small"
+                    tw="float-left relative"
+                    color="secondary"
+                    data-testid="backButton"
+                    onClick={() => {
+                        resetValues();
+                        setDialogIsOpen(false);
+                        router.push('/');
+                    }}
+                >
+                    <ArrowBackIcon tw="pr-1" /> BACK
+                </IconButton>
             </div>
-          </form>
-        </DialogContent>
-      </Box>
-    </Dialog>
-  );
+            <Box tw="w-1/3 h-1/3 border shadow rounded-lg justify-center mx-auto my-auto">
+                <DialogTitle tw="text-3xl text-secondary text-center pt-10">Create Your New Organization</DialogTitle>
+                <DialogContent tw="min-height[220px]">
+                    <form id="create-form" onSubmit={handleSubmit(submitForm)} tw="flex flex-col space-y-5 pt-4 items-center">
+                        <FormControl fullWidth error={!!errors.name}>
+                            <TextField
+                                tw="w-2/3 mx-auto"
+                                error={!!errors.name}
+                                size="small"
+                                label="Organization Name"
+                                inputProps={{ ...register('name'), 'aria-label': 'org-name' }}
+                            />
+                        </FormControl>
+                        <FormControl fullWidth error={!!errors.shortName}>
+                            <TextField
+                                tw="w-2/3 mx-auto"
+                                error={!!errors.shortName}
+                                size="small"
+                                label="Short Name"
+                                inputProps={{ ...register('shortName'), 'aria-label': 'org-shortName' }}
+                            />
+                        </FormControl>
+
+                        <div tw="flex-col">
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                type="submit"
+                                form="create-form"
+                                data-testid="createButton"
+                                disabled={createDisabled}
+                            >
+                                Create
+                            </Button>
+                        </div>
+                    </form>
+                </DialogContent>
+            </Box>
+        </Dialog>
+    );
 };
 export default Onboard;
