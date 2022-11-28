98ab53fb6cfd8eb4b4e5695ae7bcd35415a952ca
zacharyhunn <zach.hunn@trongov.us>
2022-11-28 13:10:18 -1000

    merge master into branch

---
+++ cypress/config/pipeline.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  defaultCommandTimeout: 20000,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'https://tempest.staging.dso.mil/',
+  },
+})
